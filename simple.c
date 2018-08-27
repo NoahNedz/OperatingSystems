7979f574c42e90f11921cf59ccd01c2b50ca4ac0
Noah Nedz <nmn0005@auburn.edu>
2018-08-26 22:28:06 -0500

    changes to simple.c

---
+++ simple.c
@@ -0,0 +1,26 @@
+//Author: Noah Niedzwiecki
+//ID : 903731609
+//Project 1
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+  double numbers[10] = {10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0,
+                        80.0, 90.0, 100.0};
+  double average = 0;
+  int i = 0;
+  int b = 0;
+
+  
+  while ( i < 10 ){
+    numbers[i] = sqrt(numbers[i]);
+    i++;
+  }
+
+  while ( b < 10 ){
+    average += (numbers[b]);
+    b++;
+  }
+  printf("%lf \n", average);
+  return 0;
+}
